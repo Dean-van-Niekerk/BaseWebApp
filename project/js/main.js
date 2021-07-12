3dd96fcd5ed72bc0cc0f24a52049b9b056ee7f53
Dean van Niekerk <dean.vanniekerk@icloud.com>
2021-07-12 14:40:51 +0200

    Change 12 July

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,24 @@
+
+
+function getWeather(searchQuery) {
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&appid="+apiKey;
+
+  $(".city").text();
+    $(".temp").text();
+
+  $.ajax(url,{success: function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.temp);
+  }, error: function(error){
+    $(".error-message").text("Error");
+  }})
+}
+
+function searchWeather() {
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
+}
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
